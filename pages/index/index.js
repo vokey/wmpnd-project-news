cb85db9a1202c49891373139cd607bc22cef62bd
Jinpeng LI <code@lijinpeng.cn>
2018-12-17 00:34:31 +0800

    Add categories to index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,14 @@
 //index.js
-Page()
+Page({
+  data: {
+    categories: {
+      "gn": "国内",
+      "gj": "国际",
+      "cj": "财经",
+      "yl": "娱乐",
+      "js": "军事",
+      "ty": "体育",
+      "other": "其他"
+    },
+  }
+})
