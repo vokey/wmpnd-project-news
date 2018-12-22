03394743503c0a8dacc6e7823ee32e1da57505a3
Jinpeng LI <code@lijinpeng.cn>
2018-12-22 23:27:30 +0800

    Delete unnecessary functions

--- pages/content/content.js
+++ pages/content/content.js
@@ -25,55 +25,6 @@ Page({
     }
     this.getNewsContent(this.data.id)
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  },
   
   getNewsContent(id) {
     // Try to get news detail from cache
